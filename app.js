7e12fd22e9b9680fcb0198a80ca323b2be830a2e
GopOwl <viogarden16@gmail.com>
2022-02-22 20:47:52 +0900

    Conditionals part 2

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const age = parseInt(prompt("How old are you?")); 
 
-console.log(age);
+if (isNaN(age)) {
+    console.log("Please write a number");
+}
+else {
+    console.log("나이를 입력해 주셔서 감사요~")
+}
