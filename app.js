3c5d24d70b5e3684c97fce8b62b566f10496b715
GopOwl <viogarden16@gmail.com>
2022-02-22 21:01:35 +0900

    conditionals part 3

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const age = parseInt(prompt("How old are you?")); 
 
-if (isNaN(age)) {
-    console.log("Please write a number");
+if (isNaN(age) || age < 0) {
+    console.log("Please write a real positive number");
 }
-else {
-    console.log("나이를 입력해 주셔서 감사요~")
+else if (age < 18) {
+    console.log("미성년자 술 안팔아요~");
+}
+else if (age >= 18 && age <= 50) {
+    console.log("음주 쌉가능");
+}
+else if (age > 50 && age <= 80) {
+    console.log("술보단 운동해야 할듯");
+}
+else if (age > 80) {
+    console.log("맘대로 하시죠 어르신");
 }
