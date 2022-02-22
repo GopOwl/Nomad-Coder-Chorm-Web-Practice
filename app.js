ee8ebe904075bc3fee69a3eb5113ada2f69c4090
GopOwl <viogarden16@gmail.com>
2022-02-22 19:54:48 +0900

    Returns

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const calculator = {
     add : function(a,b) {
-        console.log(a+b);
+        return a+b;
     },
     minus : function(a,b) {
-        console.log(a-b);
+        return a-b;
     },
     divide : function(a,b) {
-        console.log(a/b);
+        return a/b;
     },
     power : function(a,b) {
-        console.log(a**b);
+        return a**b;
     },
 }
 
-calculator.add(8,4);
-calculator.minus(8,4);
-calculator.divide(8,4);
-calculator.power(8,4);
+const addResult = calculator.add(8,4);
+const minusResult = calculator.minus(addResult,4);
+const divideResult = calculator.divide(addResult,minusResult);
+const powerResult = calculator.power(divideResult,addResult);
