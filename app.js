178593e794281caa81579c679ba5a36b8e3334f5
GopOwl <viogarden16@gmail.com>
2022-03-11 01:06:20 +0900

    More Events

--- app.js
+++ app.js
@@ -4,4 +4,14 @@ function handTitleClick() {
     title.style.color = 'blue';
 }
 
+function handleMouseEnter() {
+    title.innerText = "Mouse is here!"
+}
+
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone!"
+}
+
 title.addEventListener("click", handTitleClick);
+title.addEventListener("mouseenter",handleMouseEnter);
+title.addEventListener("mouseleave",handleMouseLeave);
